d1381467e278f35f80a0beeee284828bc6142198
Konstantin <kos8533@yamdex.ru>
2020-04-23 19:13:34 +0300

    Найдите  значение функции: z = ( (a – 3 ) * b / 2) + c.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,32 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Basics of software code development
+ * Найдите  значение функции: z = ( (a – 3 ) * b / 2) + c
+ */
 public class Main {
-    private int a;
-    private int b;
-    public static void main(String[] args) {
+
+    private static int a, b, c, z;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = reader.readLine();
+        String s2 = reader.readLine();
+        String s3 = reader.readLine();
+        System.out.println("Введите а " + a);
+        System.out.println("Введите b " + b);
+        System.out.println("Введите c " + c);
+        a = Integer.parseInt(s1);
+        b = Integer.parseInt(s2);
+        c = Integer.parseInt(s3);
+
+        z = ((a - 3) * b / 2) + c;
+
+        System.out.println(z);
 
     }
 }
