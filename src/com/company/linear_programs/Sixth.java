991cd8c7129eb2301c717b4be68826b112b3c3fd
Konstantin <kos8533@yamdex.ru>
2020-05-22 19:53:45 +0300

    линейные программы, задача 6 (координаты)

---
+++ src/com/company/linear_programs/Sixth.java
@@ -0,0 +1,33 @@
+package com.company.linear_programs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Для данной области составить линейную программу, которая печатает  true, если точка с координатами (х, у)
+ * принадлежит закрашенной области, и false — в противном случае
+ */
+public class Sixth {
+    private static int x, y;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите x: ");
+        String s1 = reader.readLine();
+        System.out.println("Введите y: ");
+        String s2 = reader.readLine();
+
+        x = Integer.parseInt(s1);
+        y = Integer.parseInt(s2);
+        System.out.println(coordinate(x, y));
+    }
+
+    public static boolean coordinate(int x, int y) {
+        if (y >= 0 && y <= 4 && x >= -2 && x <= 2) {
+            return true;
+        } else if (y <= 0 && y >= -3 && x >= -4 && x <= 4) {
+            return true;
+        } else return false;
+    }
+}
---
+++ src/com/company/linear_programs/SixthTest.java
@@ -0,0 +1,17 @@
+package com.company.linear_programs;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SixthTest {
+
+    @Test
+    public void coordinateCheck() {
+        Sixth sixth = new Sixth();
+        boolean result = sixth.coordinate(2, 2);
+
+        assertThat(result, is(true));
+    }
+}
