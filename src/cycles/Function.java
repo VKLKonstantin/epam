75afd17c46fa300d1139b353a5869fa4d18bbb9d
Konstantin <kos8533@yamdex.ru>
2020-05-26 18:39:22 +0300

    ветвления, задача 5 (функция)

---
+++ src/cycles/Function.java
@@ -0,0 +1,27 @@
+package cycles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Вычислить значение функции
+ */
+public class Function {
+    private static int x, y;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите x: ");
+        String s1 = reader.readLine();
+        x = Integer.parseInt(s1);
+
+        if (x <= 3) {
+            y = x * x - 3 * x + 9;
+        } else {
+            y = 1 / (x * x * x + 6);
+        }
+        System.out.println("y = " + y);
+    }
+}
