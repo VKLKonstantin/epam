79bca7acbab40a1dfa213294fb1e2b849df92e1a
Konstantin <kos8533@yamdex.ru>
2020-06-03 22:38:49 +0300

    циклы, задача 6 (Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера)

---
+++ src/cycles/Simbol.java
@@ -0,0 +1,19 @@
+package cycles;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера
+ *
+ *
+ */
+public class Simbol {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String s = "A"+"a"+"C"+"U"+"o";
+        byte[] b = s.getBytes("US-ASCII");
+        for (int i=0;i<5;i++){
+            System.out.println(b [i]);
+        }
+
+    }
+}
