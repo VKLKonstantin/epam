72f740bb534fed3665bd2f228255a0428cd9428b
Konstantin <kos8533@yamdex.ru>
2020-06-11 21:46:31 +0300

    Одномерные массивы. Задача 1. В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.

---
+++ src/algorithmization/Arrays1.java
@@ -0,0 +1,18 @@
+package algorithmization;
+
+/**
+ * В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К
+ */
+public class Arrays1 {
+    private static int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    private static int k = 3, sum = 0;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % k == 0) {
+                sum = sum + a[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
--- src/cycles/Sequence.java
+++ src/cycles/Sequence.java
@@ -7,24 +7,24 @@ import java.io.InputStreamReader;
 /**
  * Даны  числовой  ряд  и  некоторое  число  е.  Найти  сумму  тех  членов  ряда,
  * модуль  которых  больше  или  равен заданному е.
- *
  */
 public class Sequence {
     private static int e, n;
     private static double an;
-        public static void main(String[] args) throws IOException {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.println("Введите e: ");
-            String s1 = reader.readLine();
-            e = Integer.parseInt(s1);
-            System.out.println("Введите n: ");
-            String s2 = reader.readLine();
-            n = Integer.parseInt(s2);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            if (1/ Math.pow(2,n)<e) {
-                an = 1 / Math.pow(2, n) + 1 / Math.pow(3, n);
-            }
-            System.out.println(an);
+        System.out.println("Введите e: ");
+        String s1 = reader.readLine();
+        e = Integer.parseInt(s1);
+        System.out.println("Введите n: ");
+        String s2 = reader.readLine();
+        n = Integer.parseInt(s2);
+
+        if (1 / Math.pow(2, n) < e) {
+            an = 1 / Math.pow(2, n) + 1 / Math.pow(3, n);
+        }
+        System.out.println(an);
     }
 }
