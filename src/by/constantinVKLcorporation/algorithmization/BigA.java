5cd433b77bc73cf4ed8c9ff02bddd4a2998638f3
Konstantin <kos8533@yamdex.ru>
2020-06-21 17:49:35 +0300

    Одномерные массивы. Задача 5. Даны целые числа а1 ,а2 ,..., аn . Вывести на печать только те числа, для которых аi > i.

---
+++ src/by/constantinVKLcorporation/algorithmization/BigA.java
@@ -0,0 +1,21 @@
+package by.constantinVKLcorporation.algorithmization;
+
+import java.sql.SQLOutput;
+
+/**
+ * Даны целые числа а1 ,а2 ,..., аn . Вывести на печать только те числа, для которых аi > i.
+ */
+public class BigA {
+    private static int first[] = {1, 103, 5, 9, 7, 6, 0, 5, 4};
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] > i) {
+                System.out.println(first[i]);
+            }
+
+        }
+    }
+
+}
