678ed4d22db7cc8201a2b8e3044433cfc9aa85be
Konstantin <kos8533@yamdex.ru>
2020-06-29 23:03:05 +0300

    Работа над ошибками.

--- src/by/constantinVKLcorporation/module_01/branch/Brick.java
+++ src/by/constantinVKLcorporation/module_01/branch/Brick.java
@@ -9,10 +9,17 @@ import java.io.InputStreamReader;
  * отверстие.
  */
 public class Brick {
-    private static int a, b, x, y, z;
 
     public static void main(String[] args) throws IOException {
+
+        int a;
+        int b;
+        int x;
+        int y;
+        int z;
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Введите параметры прямоугольного отверстия");
         System.out.println("Введите сторону a: ");
         String s1 = reader.readLine();
@@ -25,6 +32,7 @@ public class Brick {
         String s4 = reader.readLine();
         System.out.println("Введите z: ");
         String s5 = reader.readLine();
+
         a = Integer.parseInt(s1);
         b = Integer.parseInt(s2);
         x = Integer.parseInt(s3);
@@ -32,7 +40,7 @@ public class Brick {
         z = Integer.parseInt(s5);
 
         if (x <= a && y <= b || x <= b && y <= a || z <= a && x <= b || z <= b && x <= a ||
-               z <= b && y <= a || z <= a && y <= b) {
+                z <= b && y <= a || z <= a && y <= b) {
             System.out.println("Кирпич пройдет через отверстие");
         } else {
             System.out.println("Кирпич не пройдет через отверстие");
