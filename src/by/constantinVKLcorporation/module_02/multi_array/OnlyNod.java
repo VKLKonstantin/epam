22eba439c9d511e2679ed9a03653ed7f0948b168
Konstantin <kos8533@yamdex.ru>
2020-10-03 11:20:48 +0300

    Модуль 2 Декомпозиция с использованием методов (подпрограммы) 2. Написать метод(методы) для нахождения наибольшего общего делителя четырех натуральных чисел.

--- src/by/constantinVKLcorporation/module_02/multi_array/OnlyNod.java
+++ src/by/constantinVKLcorporation/module_02/multi_array/OnlyNod.java
@@ -5,21 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Декомпозиция с использованием методов (подпрограммы)
- * 1. Написать метод(методы) для нахождения наибольшего общего делителя и наименьшего общего кратного двух
- * натуральных чисел
+ * 2. Написать метод(методы) для нахождения наибольшего общего делителя четырех натуральных чисел.
  */
-public class NokAndNod {
+public class OnlyNod {
     public static void main(String[] args) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите n1 ");
         int n1 = Integer.parseInt(read.readLine());
         System.out.println("Введите n2 ");
         int n2 = Integer.parseInt(read.readLine());
+        System.out.println("Введите n3 ");
+        int n3 = Integer.parseInt(read.readLine());
+        System.out.println("Введите n4 ");
+        int n4 = Integer.parseInt(read.readLine());
 
+        System.out.println("НОД = "+nod(nod(n1, n2), nod(n3, n4)));
 
-        System.out.println("НОК: " + nok(n1, n2));
-        System.out.println("НОД: " + nod(n1, n2));
     }
 
     public static int nod(int n1, int n2) {//нахождение НОД
@@ -33,9 +34,4 @@ public class NokAndNod {
         return nod;
     }
 
-    public static int nok(int n1, int n2) {//Нахождение НОК
-        int nod = nod(n1, n2);
-        int nok = n1 * n2 / nod;
-        return nok;
-    }
 }
